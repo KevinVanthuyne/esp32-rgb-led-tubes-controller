28f38e0dbe643d2b9ea161feb6a7588ccdab57f9
Kevin Vanthuyne <kevin.vanthuyne2@gmail.Com>
2019-10-12 13:52:42 +0200

    added methods for changing to other modes

--- src/Mode.h
+++ src/Mode.h
@@ -10,6 +10,9 @@ class Mode
 public:
     virtual ~Mode() {}
     virtual void runIteration() = 0;
+    virtual void changeToStaticMode() = 0;
+    virtual void changeToAutoMode() = 0;
+    virtual void changeToSoundMode() = 0;
 
 protected:
     Mode(std::vector<LedTube> *ledTubes) : ledTubes(ledTubes) {}
@@ -21,6 +24,14 @@ class StaticMode : public Mode
 public:
     StaticMode(std::vector<LedTube> *ledTubes) : Mode(ledTubes){};
     void runIteration();
+    void changeToStaticMode();
+    void changeToAutoMode();
+    void changeToSoundMode();
+
+private:
+    uint8_t red = 0;
+    uint8_t green = 0;
+    uint8_t blue = 0;
 };
 
 class AutoMode : public Mode
@@ -28,6 +39,10 @@ class AutoMode : public Mode
 public:
     AutoMode(std::vector<LedTube> *ledTubes) : Mode(ledTubes){};
     void runIteration();
+    // changing to other modes reset current iteration
+    void changeToStaticMode();
+    void changeToAutoMode();
+    void changeToSoundMode();
 };
 
 class SoundMode : public Mode
@@ -35,4 +50,7 @@ class SoundMode : public Mode
 public:
     SoundMode(std::vector<LedTube> *ledTubes) : Mode(ledTubes){};
     void runIteration();
+    void changeToStaticMode();
+    void changeToAutoMode();
+    void changeToSoundMode();
 };
