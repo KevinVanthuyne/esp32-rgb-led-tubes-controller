924a8ab9acc86fb3953a6ba865da7d428520573a
Kevin Vanthuyne <kevin.vanthuyne2@gmail.Com>
2019-10-21 20:44:44 +0200

    added temporary fix for weird behavior of led strips

--- src/programs/ColorSweepInToOutProgram.h
+++ src/programs/ColorSweepInToOutProgram.h
@@ -27,6 +27,7 @@ public:
         }
         strip->setPixelColor(center + currentIteration, currentColor);
         strip->setPixelColor(center - currentIteration, currentColor);
+        delay(1); // delay to make sure all pixel data is processed correctly, since there is some trouble with the NeoPixel library and the ESP32
         strip->show();
 
         currentIteration++;
